4883f551ad2527bfb1689283b2ad0c27e0c6eb35
Nursena <nursena.atalar14@gmail.com>
2021-01-23 00:46:02 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public Customer Add( Customer customer) 
+        {
+           Console.WriteLine(" Eklemek istediğiniz müşteri adı giriniz");
+           customer.Name =  Console.ReadLine();
+           Console.WriteLine(customer.Name+" isimli müşteri eklendi.");
+            return customer; 
+        }
+        public Customer Delete(Customer customer)
+        {
+            Console.WriteLine(" Silmek istediğiniz müşteri adı giriniz");
+            customer.Name = Console.ReadLine();
+            Console.WriteLine(customer.Name + " isimli müşteri silindi.");
+            return customer;
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = "123456789";
+            customer1.Name = "Ahmet";
+            customer1.Surname = "Yılmaz";
+            customer1.Age = 32;
+
+            Customer customer2 = new Customer();
+            customer2.Id = "987654321";
+            customer2.Name = "Ayşe";
+            customer2.Surname = "Korkmaz";
+            customer2.Age = 45;
+
+            Customer customer3 = new Customer { Id = "246824680" , Name= "Hasan" ,Surname= "Dede" ,Age=65};
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+            Console.WriteLine("....Mevcut Müşterilerimiz....");
+            foreach (Customer custom in customers)
+            {
+                Console.WriteLine(custom.Name + " "+ custom.Surname + " / " +custom.Age );
+            }
+
+            CustomerManager customerManager = new CustomerManager();
+
+            Console.WriteLine(".....Yeni Müşteri Ekle.....");
+            customerManager.Add(new Customer());
+
+            Console.WriteLine("....Müşteri Silme....");
+            customerManager.Delete(new Customer());
+
+
+
+
+
+        }
+    }
+}
